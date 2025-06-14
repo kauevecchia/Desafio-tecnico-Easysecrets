84fb23a3953310afca4fb516ebe470d9fa17314e
Kauê <kauevecchia@hotmail.com>
2025-06-14 16:19:59 -0300

    feat: create ThemeProvider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { ThemeProvider } from "./components/ThemeProvider";
 import { Router } from "./Router";
 import { BrowserRouter } from "react-router-dom";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+    >
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
